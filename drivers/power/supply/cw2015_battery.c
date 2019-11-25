be49d659a9c33a88a10e3f480e26912009add8c2
Tobias Schramm <tobias@t-sys.eu>
2019-11-25 21:05:04 +0100

    drivers/power: cw2015_battery: Add design charge_full_design_property
    Thanks to schaecsn for this suggestion

--- drivers/power/supply/cw2015_battery.c
+++ drivers/power/supply/cw2015_battery.c
@@ -655,6 +655,7 @@ static int cw_battery_get_property(struct power_supply *psy,
 		break;
 
 	case POWER_SUPPLY_PROP_CHARGE_FULL:
+	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
 		val->intval = cw_bat->plat_data.design_capacity * 1000;
 		break;
 
@@ -694,6 +695,7 @@ static enum power_supply_property cw_battery_properties[] = {
 	POWER_SUPPLY_PROP_TECHNOLOGY,
 	POWER_SUPPLY_PROP_CHARGE_COUNTER,
 	POWER_SUPPLY_PROP_CHARGE_FULL,
+	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
 	POWER_SUPPLY_PROP_TEMP,
 	POWER_SUPPLY_PROP_CURRENT_NOW,
 };
